48ec4bffb1840a6e0ea470864f3208210c5d6f71
rdalin82 <Robert.Dalin@gmail.com>
2015-08-24 20:28:13 -0500

    factoral

--- recursion/factoral.rb
+++ recursion/factoral.rb
@@ -1,6 +1,6 @@
 class Recursion 
 	def factoral(n)
-		return n if n==1
+		return n if n <= 1
 		n * factoral(n-1)
 	end 
 end 
--- recursion/factoral_spec.rb
+++ recursion/factoral_spec.rb
@@ -13,4 +13,11 @@ describe Recursion do
   it "should return 2 if 2" do 
   	assert_equal 2, recursion.factoral(2)
   end 
+  it "should return 0 if 0" do 
+  	assert_equal 0, recursion.factoral(0)
+  end 
+
+  it "should return __ if 10" do 
+  	assert_equal 3628800, recursion.factoral(10)
+  end 
 end 

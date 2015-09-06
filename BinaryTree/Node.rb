70c864b5a00287f69ec26251694bece5bde7e6a8
rdalin82 <Robert.Dalin@gmail.com>
2015-09-06 09:57:58 -0500

    started BST class

--- BinaryTree/Node.rb
+++ BinaryTree/Node.rb
@@ -1,27 +1,44 @@
 class Node
-	attr_reader :left, :right 
-	attr_accessor :value
+	attr_accessor :left, :right, :parent, :value
 	def initialize(args={})
 		@value = args[:value]
 		@left = args[:left]
 		@right = args[:right]
-	end 
-
-	def add(node)
-
-		if node.value > @value 
-			if @right
-		  	self.right.add(node) 
-			else 
-				@right = node 
-			end 
-		elsif node.value < @value 
-			if @left 
-				self.left.add(node)
-			else 
-			@left = node 
-		end 
-		else 
-		end 
+		@parent = args[:parent]
 	end 
 end 
+
+class BinarySearchTree
+	attr_accessor :root
+	def initialize(root)
+		@root = root
+	end 
+
+	def build_tree(array)
+		array.each do |value|
+			self.add(value)
+		end
+		self  
+	end 
+
+	def add(value, node=@root)
+		if value > node.value
+			if node.right
+				n2 = node.right 
+				self.add(value, n2)
+			else 
+				node.right = Node.new({:parent => node, :value=> value})
+			end 
+		elsif value < node.value
+			if node.left 
+				n3 = node.left 
+				self.add(value, n3)
+			else 
+				node.left = Node.new({:parent=> node, :value=>value})
+			end 
+		else 
+			node 
+		end 
+	end 
+
+end
